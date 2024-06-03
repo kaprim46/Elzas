0247e2b19b57b3eac427eb769df924b3d0aaaa8d
marouanekrizi <kaprim41@outlook.com>
2024-06-03 13:31:09 +0100

    End of section 20

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -9,7 +9,6 @@ import { CoreModule } from './core/core.module';
 import { HomeModule } from './home/home.module';
 import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 import { LoadingInterceptor } from './core/interceptors/loading.interceptor';
-import { CheckoutComponent } from './checkout/checkout.component';
 import { JwtInterceptor } from './core/interceptors/jwt.interceptor';
 
 @NgModule({
